9c07091af5c2de096d26faa293f5403712ef05a3
Limson91 <matteo.a.colautti@gmail.com>
2023-10-13 17:02:28 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const gridDOMElement = document.querySelector('.grid');
+console.log(gridDOMElement);
+
+// for (let i = 0; i < 100; i++) {
+//     const n = i + 1;
+//     console.log(n);
+
+//     //creo un elemento html con template literal
+//     const htmlCell = `<div class="class">${n}</div>`
+//     console.log(htmlCell);
+
+//     // lo riassegno alla griglia con la ripetizione +=
+//     gridDOMElement.innerHTML += htmlCell
+// }
+
+const btnDOMElement = document.getElementById('play-btn');
+console.log(btnDOMElement);
