0622458a7388972a41947cd464c37d1402146f7e
Limson91 <matteo.a.colautti@gmail.com>
2023-10-13 17:23:08 +0200

    add static grid creation

--- js/app.js
+++ js/app.js
@@ -15,3 +15,21 @@ console.log(gridDOMElement);
 
 const btnDOMElement = document.getElementById('play-btn');
 console.log(btnDOMElement);
+
+// al click su start generare una griglia 10x10
+btnDOMElement.addEventListener('click', function() {
+    gridDOMElement.innerHTML = ''; //se click più volte sul btn start non genera altre griglie.
+
+    //creare 100 caselle in ciclo for
+    for (let i = 0; i < 100; i++) {
+    const n = i + 1;
+    console.log(n);
+
+    //creare le caselle inserendo l'index con template literal html
+    const htmlCell = `<div class="cell">${n}</div>`
+    console.log(htmlCell);
+
+    // riassegnare le caselle alla griglia con la ripetizione +=
+    gridDOMElement.innerHTML += htmlCell
+    }
+})
